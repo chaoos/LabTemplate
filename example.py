fixed stuff with $-signs

example.py
@@ -39,6 +39,7 @@ k_1 = lambda T: np.polyval(np.polyfit([250.0, 400.0], [401.0, 391.0], 1), T) # W
 k_2 = lambda T: np.polyval(np.polyfit([275.0, 400.0], [21.9, 26.6], 1), T) # W/mK
 F_1 = np.pi*(0.5*d_1)**2# m^2
 F_2 = np.pi*(0.5*d_2)**2# m^2
+deltaI_T = 8.5*10**-6
 
 # linear polyfit of the reference table of the type k thermocouple
 C = np.arange(-10, 21, 1) # °C
@@ -54,7 +55,7 @@ for T_b in T_bs:
 
 	# fetch the measured data from the csv file
 	V_S = hp.fetch('data/example_data_' + Temp + '.csv', 'V_Shunt [V]', 1.0*10**-3)
-	I_T = hp.fetch('data/example_data_' + Temp + '.csv', 'I_T [A]', 0.1*10**-6)
+	I_T = hp.fetch('data/example_data_' + Temp + '.csv', 'I_T [A]', deltaI_T)
 	V_p = hp.fetch('data/example_data_' + Temp + '.csv', 'V_p [V]', 1.0*10**-3)
 
 	# do some calculation
@@ -110,8 +111,15 @@ for T_b in T_bs:
 		plt.grid(True)
 		plt.xlabel(r'$I$ [A]')
 
+	V_S = np.concatenate((np.array([r"$V_{Shunt} [mV]$"]), V_S*10**3))
+	I_T = np.concatenate((np.array([r"$I_T [micro A]$"]), I_T*10**6))
+	V_p = np.concatenate((np.array([r"$V_p [mV]$"]), V_p*10**3))
+	I   = np.concatenate((np.array([r"$I [A]$"]), I))
+	arr = np.array([I, V_S, I_T, V_p]).T
+	hp.replace("table"+Temp, arr)
+
 # save the plots im format .fmt
-fmt = "eps"
+fmt = "png"
 plt.figure(0)
 plt.savefig("plots/Pi12V_vs_I." + fmt)
 
@@ -146,15 +154,9 @@ hp.replace("polyfit1", "f(x) = " + hp.fitline(coeffs, 1))
 hp.replace("polyfit2", hp.fitline(coeffs, 2))
 hp.replace("polyfit3", hp.fitline(coeffs, 4))
 hp.replace("polyfit4", hp.fitline(coeffs))
-
-
-V_S = np.concatenate((np.array([r"$V_{Shunt} [V]$"]), hp.fetch('data/example_data_80.csv', 'V_Shunt [V]', 1.0*10**-3)))
-I_T = np.concatenate((np.array([r"$I_T [A]$"]), hp.fetch('data/example_data_80.csv', 'I_T [A]', 0.1*10**-6)))
-V_p = np.concatenate((np.array([r"$V_p [V]$"]), hp.fetch('data/example_data_80.csv', 'V_p [V]', 1.0*10**-3)))
-I   = np.concatenate((np.array([r"$I ~ [A]$"]), hp.fetch('data/example_data_80.csv', 'I ~ [A]')))
-arr = np.array([I, V_S, I_T, V_p]).T
-
-hp.replace("table", arr)
+hp.replace("Name", "Roman Gruber")
+hp.replace("Experiment", "Peltier Effect")
+hp.replace("deltaI_T", deltaI_T)
 
 hp.compile()
 

helpers.py
@@ -49,10 +49,9 @@ def equal (A, B, tol):
 def compile (pdffile = "main", converter = "pdflatex"):
 	#call([converter, '-jobname=' + pdffile, 'log/intermediate.tex'], stdin=None, stdout=None, stderr=None)
 	p = Popen([converter, '-halt-on-error', '-jobname=' + pdffile, 'log/intermediate.tex'], stdin=None, stdout=PIPE, stderr=PIPE)
-	output, err = p.communicate(b"input data that is passed to subprocess' None")
+	output, err = p.communicate(b"input data that is passed to subprocess' stdin")
 	rc = p.returncode
 
-	print(rc)
 	if rc != 0:
 		print("Compilation of main.tex failed:")
 		print(output)
@@ -111,14 +110,12 @@ def fmt_number (nr, sign = '.2f'):
 
 	return s
 
-def fmt_table (m, fmt=True):
+def fmt_table (m):
+	#m = format(np.atleast_2d(m)) if fmt else np.atleast_2d(m)
 	m = np.atleast_2d(m)
 	t = []
 	for i, v in enumerate(m):
-		if fmt:
-			t.append(' & '.join(format(x) for x in v))
-		else:
-			t.append(' & '.join(str(x) for x in v))
+		t.append(' & '.join(str(x) for x in v))
 	
 	tbl = ''
 	tbl += t[0] + '\\\\\n\\hline\n\hline\n'
@@ -126,15 +123,35 @@ def fmt_table (m, fmt=True):
 	tbl += '\\\\\n'.join(str(x) for x in t)
 	return tbl
 
-def format(s, fmt=True):
+def format(s, fmt=False):
 	if isinstance(s, str): # it is a string
 		return s
+
 	elif hasattr(s, "__len__"): # it is an array
-		return fmt_table(s, fmt)		
+		s = np.atleast_2d(s)
+		for i, a in enumerate(s):
+			for j, b in enumerate(a):
+				s[i][j] = format(b, True)
+		return fmt_table(s)		
 	elif (hasattr(s, 'n')): # it has uncertainties
-		return "$" + fmt_number(s) + "$" if fmt else str(s)
+		return "$" + fmt_number(s) + "$" if fmt else fmt_number(s)
+	elif isinstance(s, int) or isinstance(s, float):
+		return "$" + fmt_number(s) + "$" if fmt else fmt_number(s)
 	else: # it is a regular number (float, int)
-		return "$" + fmt_number(s) + "$" if fmt else str(s)
+		return str(s)
+
+'''
+def format(el, fmt=True):
+	print(el)
+	if isinstance(el, list):
+		exit()
+		for i, a in enumerate(el):
+			for j, b in enumerate(el[i]):
+				el[i][j] = format_one(a, fmt)
+		return el
+	else:
+		return format_one(el, fmt)
+'''
 
 def fitline(C, deg=None):
 	if (deg == None):
@@ -155,7 +172,7 @@ def fitline(C, deg=None):
 
 
 first = True
-def replace(s, r, fmt=True, texfile = "main.tex"):
+def replace(s, r, fmt=False, texfile = "main.tex"):
 	global first
 	if first:
 		file = texfile