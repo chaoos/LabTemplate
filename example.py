f99afa4bb323618dff1a9e6e4969f379f5db27e0
chaoos <p1020389@yahoo.com>
2018-10-08 09:10:28 +0200

    initial commit

---
+++ example.py
@@ -0,0 +1,156 @@
+# coding: utf-8
+
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
+from scipy import interpolate
+from scipy.optimize import curve_fit
+from scipy.interpolate import UnivariateSpline
+from scipy.optimize import fsolve
+from decimal import Decimal
+import pandas as pd
+from uncertainties import ufloat, unumpy
+from uncertainties.umath import *
+from subprocess import call
+import os
+#from helpers import *
+import helpers as hp
+
+font = {'family' : 'normal',
+        'size'   :  16}
+matplotlib.rc('font', **font)
+
+# Custom code starts here
+
+print(type(unumpy.uarray([2], 0.1)))
+
+# some initial contants with their errors
+T_bs = np.array([30.0, 50.0, 80.0, 110.0]) + 273.15 # °K
+P_1 = ufloat(200.0, 0.25*200.0) # Ohm
+R_1 = ufloat(1200.0, 120.0) # Ohm
+R_2 = ufloat(389*10**-3, 0.1*389*10**-3) # Ohm
+I_Shunt	= ufloat(20.0, 0.1) # A
+V_Shunt = ufloat(50.0*10**-3, 0.1*50.0*10**-3) # V
+L_1 = 0.05 # m
+L_2 = 0.05 # m
+d_1 = 2.03*10**-3 # m
+d_2 = 7.04*10**-3 # m
+rho_1 = lambda T: np.polyval(np.polyfit(np.array([293.0, 353.0]), np.array([1.68, 2.06])*10**-8, 1), T) # linear polyfit
+rho_2 = lambda T: 44.0*10**-8 # Ohm meters
+k_1 = lambda T: np.polyval(np.polyfit([250.0, 400.0], [401.0, 391.0], 1), T) # W/mK
+k_2 = lambda T: np.polyval(np.polyfit([275.0, 400.0], [21.9, 26.6], 1), T) # W/mK
+F_1 = np.pi*(0.5*d_1)**2# m^2
+F_2 = np.pi*(0.5*d_2)**2# m^2
+
+# linear polyfit of the reference table of the type k thermocouple
+C = np.arange(-10, 21, 1) # °C
+V = np.array([-0.392, -0.353, -0.314, -0.275, -0.236, -0.197, -0.157, -0.118, -0.079, -0.039, 0.000, 0.039, 0.079, 0.119, 0.158, 0.198, 0.238, 0.277, 0.317, 0.357, 0.397, 0.437, 0.477, 0.517, 0.557, 0.597, 0.637, 0.677, 0.718, 0.758, 0.798]) * 10**-3 # V
+coeffs = hp.upolyfit(V, C, 1)
+p = lambda x: np.polyval(coeffs, x)
+f = lambda x: np.exp(2*x) + np.sin(2*np.pi*x^2) + R_2
+
+# create plots for all T's separately
+for T_b in T_bs:
+
+	Temp = str(int(T_b-273.15)) # T in °C
+
+	# fetch the measured data from the csv file
+	V_S = hp.fetch('data/example_data_' + Temp + '.csv', 'V_Shunt [V]', 1.0*10**-3)
+	I_T = hp.fetch('data/example_data_' + Temp + '.csv', 'I_T [A]', 0.1*10**-6)
+	V_p = hp.fetch('data/example_data_' + Temp + '.csv', 'V_p [V]', 1.0*10**-3)
+
+	# do some calculation
+	n = int(V_p.size/2) # n=4, half of the number of measurements
+	V_T = R_2*I_T
+	I = V_S*I_Shunt/V_Shunt
+	V_p_mean = 0.5*(np.abs(np.flipud(V_p[0:n])) + np.abs(V_p[n:]))
+	I_mean = 0.5*(np.abs(np.flipud(I[0:n])) + np.abs(I[n:]))
+	deltaT = p(V_T)
+	deltaTminus = np.flipud(deltaT[0:n])
+	deltaTplus = deltaT[n:]
+	R_tot = L_1*rho_1(T_b)/F_1 + L_2*rho_2(T_b)/F_2
+
+	Pi12V = 0.5*V_p_mean*(deltaTplus - deltaTminus)/(deltaTplus + deltaTminus)
+	Pi12I = ((k_1(T_b)*F_1)/L_1 + (k_2(T_b)*F_2)/L_2)*(deltaTplus - deltaTminus)/(2*I_mean)
+
+	### PLOTS ###
+
+	# I - Pi12V
+	plt.figure(0)
+	plt.errorbar(hp.nominal(I_mean), hp.nominal(Pi12V)*10**3, xerr=hp.stddev(I_mean), yerr=hp.stddev(Pi12V)*10**3, label=r'' + Temp + '°C')
+	plt.ylabel(r'$\Pi_{12}^{(V)}$ [mV]')
+
+	# I - Pi12I
+	plt.figure(1)
+	plt.errorbar(hp.nominal(I_mean), hp.nominal(Pi12I)*10**3, xerr=hp.stddev(I_mean), yerr=hp.stddev(Pi12I)*10**3, label=r'' + Temp + '°C')
+	plt.ylabel(r'$\Pi_{12}^{(I)}$ [mV]')
+
+	# I - dT
+	plt.figure(2)
+	plt.errorbar(hp.nominal(I), hp.nominal(deltaT), xerr=hp.stddev(I), yerr=hp.stddev(deltaT), label=r'' + Temp + '°C')
+	plt.ylabel(r'$\Delta T^{\pm}$ [°K]')
+
+	# I - (dT+ - dT-)
+	plt.figure(3)
+	plt.errorbar(hp.nominal(I_mean), hp.nominal(deltaTplus - deltaTminus), xerr=hp.stddev(I_mean), yerr=hp.stddev(deltaTplus - deltaTminus), label=r'' + Temp + '°C')
+	plt.ylabel(r'$\Delta T^{+} - \Delta T^{-}$ [°K]')
+
+	# I - (dT+ + dT-)
+	plt.figure(4)
+	plt.errorbar(hp.nominal(I_mean), hp.nominal(deltaTplus + deltaTminus), xerr=hp.stddev(I_mean), yerr=hp.stddev(deltaTplus + deltaTminus), label=r'' + Temp + '°C')
+	plt.ylabel(r'$\Delta T^{+} + \Delta T^{-}$ [°K]')
+
+	# attributes of all plots
+	for i in [0, 1, 2, 3, 4, 5]:
+		plt.figure(i)
+		plt.legend(loc="best")
+		plt.grid(True)
+		plt.xlabel(r'$I$ [A]')
+
+# save the plots im format .fmt
+fmt = "eps"
+plt.figure(0)
+plt.savefig("plots/Pi12V_vs_I." + fmt)
+
+plt.figure(1)
+plt.savefig("plots/Pi12I_vs_I." + fmt)
+
+plt.figure(2)
+plt.savefig("plots/dT_vs_I." + fmt)
+
+plt.figure(3)
+plt.savefig("plots/dTp-dTm_vs_I." + fmt)
+
+plt.figure(4)
+plt.savefig("plots/dTp+dTm_vs_I." + fmt)
+
+
+# replace the markers in main.tex with the formatted variables/fitlines/tables etc...
+hp.replace("EQ1", "f(x) = " + hp.fitline([2, 5], 1))
+hp.replace("variable1", ufloat(12345678923.3, 345678923))
+hp.replace("variable2", ufloat(69.911212, 6.721212))
+hp.replace("variable3", ufloat(0.00992123123, 0.00095123123))
+hp.replace("variable4", 0.00992123123)
+hp.replace("variable5", ufloat(1, 10**-5))
+hp.replace("variable6", 0.0)
+hp.replace("variable7", 0)
+hp.replace("variable8", 23)
+hp.replace("variable9", ufloat(10, 0))
+hp.replace("variable10", 10**23)
+hp.replace("polyfit1", "f(x) = " + hp.fitline(coeffs, 1))
+hp.replace("polyfit2", hp.fitline(coeffs, 2))
+hp.replace("polyfit3", hp.fitline(coeffs, 4))
+hp.replace("polyfit4", hp.fitline(coeffs))
+
+
+V_S = np.concatenate((np.array([r"$V_{Shunt} [V]$"]), hp.fetch('data/example_data_80.csv', 'V_Shunt [V]', 1.0*10**-3)))
+I_T = np.concatenate((np.array([r"$I_T [A]$"]), hp.fetch('data/example_data_80.csv', 'I_T [A]', 0.1*10**-6)))
+V_p = np.concatenate((np.array([r"$V_p [V]$"]), hp.fetch('data/example_data_80.csv', 'V_p [V]', 1.0*10**-3)))
+I   = np.concatenate((np.array([r"$I ~ [A]$"]), hp.fetch('data/example_data_80.csv', 'I ~ [A]')))
+arr = np.array([I, V_S, I_T, V_p]).T
+
+hp.replace("table", arr)
+
+hp.compile()
+
+#plt.show()
---
+++ helpers.py
@@ -0,0 +1,166 @@
+# coding: utf-8
+
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
+from scipy import interpolate
+from scipy.optimize import curve_fit
+from scipy.interpolate import UnivariateSpline
+from scipy.optimize import fsolve
+from decimal import Decimal
+import pandas as pd
+from uncertainties import ufloat, unumpy
+from uncertainties.umath import *
+from subprocess import call
+import os
+
+'''
+    Fetch data from a csv file
+    @param {string} file        - csv file containing data
+    @param {string} col         - column header name
+    @param {float|array} err    - error of the data set 
+    @return {numpy.ndarray}		- numpy array containing the data (and their errors if given)
+'''
+def fetch (file, col, err=0):
+	df = pd.read_csv(file)
+	if err == 0:
+		return np.array(df[col])
+	else:
+		return unumpy.uarray(df[col], np.zeros(df[col].size) + err)
+
+def nominal (a):
+	arr = np.array(a)
+	for i, v in enumerate(arr):
+		arr[i] = v.n if hasattr(v, 'n') else v
+	return arr
+
+def stddev (a):
+	arr = np.array(a)
+	for i, v in enumerate(arr):
+		arr[i] = v.std_dev if hasattr(v, 'std_dev') else 0.0
+	return arr
+
+def equal (A, B, tol):
+	for d in np.abs(A - B) - tol:
+		if d > 0:
+			return False
+	return True
+
+def compile (pdffile = "main", converter = "pdflatex"):
+	call([converter, '-jobname=' + pdffile, 'log/intermediate.tex'], stdin=None, stdout=None, stderr=None)
+	os.remove(pdffile + ".aux")
+	os.rename(pdffile + ".log", "log/" + pdffile + ".log")
+
+'''
+    Format a number (with its error if given) in latex
+    @param {int|float|ufloat} nr	- number
+    @param {string} sign			- format
+    @return {string}				- formatted number in latex
+'''
+def fmt_number (nr, sign = '.2f'):
+
+	s = "NaN"
+	fmtNull = (r"{0:"+sign+r"}").format(0.0)
+	if (np.abs(nr) > 9999 or np.abs(nr) < 0.1) and nr != 0:
+		if hasattr(nr, 'n'):
+			float_str = "{0:e}".format(nr.n)
+			base, exponent = float_str.split("e")
+			base, exponent = float(base), int(exponent)
+			err = nr.s / 10**exponent
+			if (nr.n == 10**exponent):
+				s = (r"10^{{{0}}} \pm {1}").format(exponent, fmt_number(nr.s, sign))
+			elif (r"{0:"+sign+r"}").format(err) == fmtNull:
+				s = (r"{0:"+sign+r"} \times 10^{{{1}}} \pm {2}").format(base, exponent, fmt_number(nr.s, sign))
+			else:
+				s = (r"\left( {0:"+sign+r"} \pm {1:"+sign+r"} \right) \times 10^{{{2}}}").format(base, err, exponent)
+
+		else:
+			float_str = "{0:e}".format(nr)
+			base, exponent = float_str.split("e")
+			base, exponent = float(base), int(exponent)
+			if (nr == 10**exponent):
+				s = (r"10^{{{0}}}").format(exponent)
+			else:
+				s = (r"{0:"+sign+r"} \times 10^{{{1}}}").format(base, exponent)
+	else:
+		if hasattr(nr, 'n'):
+			if (nr.s == 0.0):
+				s = (r"{0:"+sign+r"}").format(nr.n)
+			elif (r"{0:"+sign+r"}").format(nr.s) == fmtNull:
+				s = (r"{0:"+sign+r"} \pm {1}").format(nr.n, fmt_number(nr.s, sign))
+			else:
+				s = (r"{0:"+sign+r"} \pm {1:"+sign+r"}").format(nr.n, nr.s)
+		else:
+			if isinstance(nr, int):
+				s = str(nr)
+			else:
+				s = (r"{0:"+sign+r"}").format(nr)
+
+	return s
+
+def fmt_table (m, fmt=True):
+	m = np.atleast_2d(m)
+	t = []
+	for i, v in enumerate(m):
+		if fmt:
+			t.append(' & '.join(format(x) for x in v))
+		else:
+			t.append(' & '.join(str(x) for x in v))
+	
+	tbl = ''
+	tbl += t[0] + '\\\\\n\\hline\n\hline\n'
+	t.pop(0)
+	tbl += '\\\\\n'.join(str(x) for x in t)
+	return tbl
+
+def format(s, fmt=True):
+	if isinstance(s, str): # it is a string
+		return s
+	elif hasattr(s, "__len__"): # it is an array
+		return fmt_table(s, fmt)		
+	elif (hasattr(s, 'n')): # it has uncertainties
+		return "$" + fmt_number(s) + "$" if fmt else str(s)
+	else: # it is a regular number (float, int)
+		return "$" + fmt_number(s) + "$" if fmt else str(s)
+
+def fitline(C, deg=None):
+	if (deg == None):
+		deg = len(C)-1
+	pf = ""
+	for i, c in (enumerate(C)):
+		e = len(C) - (i+1) # exponent
+		if e > deg:
+			continue
+		if c > 0 and deg != e:
+			pf += "+"
+		pf += fmt_number(c)
+		if e == 1:
+			pf += " x"
+		elif e != 0:
+			pf += " x^{" + str(len(C) - (i+1)) +"}"
+	return pf
+
+
+first = True
+def replace(s, r, fmt=True, texfile = "main.tex"):
+	global first
+	if first:
+		file = texfile
+	else:
+		file = 'log/intermediate.tex'
+	# Read in the file
+	with open(file, 'r') as file :
+		filedata = file.read()
+
+	# Replace the target string
+	filedata = filedata.replace("[[" + s + "]]", format(r, fmt))
+
+	# Write the file out again
+	with open('log/intermediate.tex', 'w') as file:
+		file.write(filedata)
+
+	first = False
+
+def upolyfit(x, y, deg, **kwargs):
+	c, cov = np.polyfit(x, y, deg, cov=True, **kwargs)
+	return unumpy.uarray(c, np.diagonal(cov))
